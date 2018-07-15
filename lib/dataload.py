37071c7eb82c736773a6d6b92e1c2f571a978ef7
Michael Patini <michaelpatini@Michaels-MacBook-Pro.local>
2018-07-14 20:49:57 -0700

    Updated todo for commandline application Created lib folder for program modules completed dataload and improc modules v.1

---
+++ lib/dataload.py
@@ -0,0 +1,48 @@
+# Imports
+import json
+import torch
+from torchvision import datasets, transforms, models
+
+
+def load_dir(path):
+    """
+    Loads the image to be used in training, validating, and testing.
+    Input: path as a String, to the parent folder
+    Output: three directories, that are subfolders of the parent
+    """
+    train_dir = path + '/train'
+    valid_dir = path + '/valid'
+    test_dir = path + '/test'
+    return train_dir, valid_dir, test_dir
+
+def dataloader(train_dir, valid_dir, test_dir, transforms):
+    """
+    Loads image data from directories
+    Input: Three dirs train, valid, and test, and a tuple -> 
+    (train, test) transforms
+    Output: Three dataloaders, trainloader, validloader testloader
+    Example of usage:
+        image, label = next(iter(train_loader))
+    """
+
+    # Load the data
+    train_data = datasets.ImageFolder(train_dir, transform=transforms[0])
+    valid_data = datasets.ImageFolder(valid_dir, transform=transforms[1])
+    test_data = datasets.ImageFolder(test_dir, transform=transforms[1])
+
+    # Create dataloaders
+    train_loader = torch.utils.data.DataLoader(train_data, batch_size=64, shuffle=True)
+    valid_loader = torch.utils.data.DataLoader(valid_data, batch_size=64, shuffle=True)
+    test_loader = torch.utils.data.DataLoader(test_data, batch_size=32, shuffle=True)
+
+    return train_loader, valid_loader, test_loader
+
+def labels(path):
+    """
+    Loads categorical labels for image data from json file
+    Input: path to json file
+    Output: dictionary
+    """
+    with open('cat_to_name.json', 'r') as f:
+        cat_to_name = json.load(f)
+    return cat_to_name
---
+++ lib/improc.py
@@ -0,0 +1,71 @@
+# Imports
+import matplotlib.pyplot as plt
+import torch
+from torchvision import datasets, transforms, models
+from PIL import Image
+import numpy as np
+
+def group_transform(train=True, test=True):
+    """
+    Defines image transforms for training and testing.
+    Output: tuple -> train_transforms, test_transforms
+    """
+    train_transforms = transforms.Compose([transforms.Resize(256),
+                                       transforms.RandomResizedCrop(224),
+                                       transforms.RandomRotation(30),
+                                       transforms.RandomGrayscale(.1),
+                                       transforms.RandomHorizontalFlip(),
+                                       transforms.ToTensor(),
+                                       transforms.Normalize(
+                                            [0.485, 0.456, 0.406],
+                                            [0.229, 0.224, 0.225])])
+    test_transforms = transforms.Compose([transforms.Resize(256),
+                                        transforms.CenterCrop(224),
+                                        transforms.ToTensor(),
+                                        transforms.Normalize(
+                                            [0.485, 0.456, 0.406],
+                                            [0.229, 0.224, 0.225])])
+    return train_transforms, test_transforms
+
+def process_image(image_path):
+    ''' Scales, crops, and normalizes a PIL image for a PyTorch model
+        Input: path to individual image.
+        Output: Pil Image
+    '''
+    image = Image.open(image_path)
+    test_transforms = transforms.Compose([transforms.Resize(256),
+                                          transforms.CenterCrop(224),
+                                          transforms.ToTensor(),
+                                          transforms.Normalize(
+                                            [0.485, 0.456, 0.406],
+                                            [0.229, 0.224, 0.225])])
+    image = test_transforms(image)
+    return image
+
+def imshow(image, ax=None, title=None):
+    """
+    Imshow for Tensor.
+    Input: image processed by process_image(),
+    an optional pyplot ax and title
+    Output: image display to user
+    """
+    if ax is None:
+        fig, ax = plt.subplots()
+    
+    # PyTorch tensors assume the color channel is the first dimension
+    # but matplotlib assumes is the third dimension
+    image = image.numpy().transpose((1, 2, 0))
+    
+    # Undo preprocessing
+    mean = np.array([0.485, 0.456, 0.406])
+    std = np.array([0.229, 0.224, 0.225])
+    image = std * image + mean
+    
+    # Image needs to be clipped between 0 and 1 or it looks like noise when displayed
+    image = np.clip(image, 0, 1)
+            
+    ax.imshow(image)
+    ax.axes.get_xaxis().set_visible(False)
+    ax.axes.get_yaxis().set_visible(False)
+    return ax
+
