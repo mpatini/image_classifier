7ae86a3679e0d580dd0a3ad7070fbe169d9c51ba
Michael Patini <michaelpatini@Michaels-MacBook-Pro.local>
2018-07-15 00:56:56 -0700

    finished most of deeplearn module except for providing 2 architectures upon init of model and classifier

---
+++ lib/deeplearn.py
@@ -0,0 +1,167 @@
+import torch
+from torch import optim
+from torch import nn
+import torch.nn.functional as F
+from torchvision import datasets, transforms, models
+from collections import OrderedDict
+
+# TODO define two arch's
+def init_model(arch, units=(500,102)):
+    """
+    Initializes a pytorch pretrained deep learning model
+    Input: 1 of 2 architectures as a String, tuple ->
+    (int, int) number of desired units for one
+    hidden layer and the output layer
+    Output: model
+    """
+    model = models.densenet121(pretrained=True)
+
+    # Freeze parameters so no backprop
+    for param in model.parameters():
+        param.requires_grad = False
+
+    # Replace deep learning model's classifier
+    model.classifier = classifier(model, units)
+
+    return model
+
+# TODO define classifier input for two arch's
+def classifier(arch, units):
+    """
+    Defines a classifier for a pretrained model
+    Input: 1 of 2 architectures as a String, tuple ->
+    (int, int) number of desired units for one
+    hidden layer and the output layer
+    Output: classifier as an ordered dict
+
+    """
+    classifier = nn.Sequential(OrderedDict([
+                            ('fc1', nn.Linear(1024, units[0])),
+                            ('relu1', nn.ReLU()),
+                            ('fc2', nn.Linear(units[0], units[1])),
+                            ('output', nn.LogSoftmax(dim=1))
+                            ]))
+    return classifier
+
+def train_deep(model, train_loader, valid_loader, criterion, lr=0.001, epochs=3, device='cpu'):
+    """
+    Define deep learning model training
+    Input: initialized deep learning model,
+    train and valid loader as iterators,
+    criterion for calculating loss,
+    learning rate as float, number of training epochs,
+    device 'cpu' or 'cuda' as Strings
+    Output: Prints regularly epoch, loss, test loss, and
+    test accuracy. Trains model in place
+    """
+
+    # Train deep learning network
+    print_every = 40
+    steps = 0
+
+    # Define optimizer
+    optimizer = optim.Adam(model.classifier.parameters(), lr=lr)
+    
+    # Change devide
+    if device == 'cuda':
+        model.to('cuda')
+        
+    for e in range(epochs):
+        model.train()
+        running_loss = 0
+        for images, labels in train_loader:
+            steps += 1
+            
+            if device == 'cuda':
+                images, labels = images.to('cuda'), labels.to('cuda')
+                
+            optimizer.zero_grad()
+
+            # Forward and backward passes
+            output = model.forward(images)
+            loss = criterion(output, labels)
+            loss.backward()
+            optimizer.step()
+        
+            running_loss += loss.item()
+
+            if steps % print_every == 0:
+                model.eval()
+                with torch.no_grad():
+                    test_loss, accuracy = validation(model, valid_loader, criterion, device)
+                print("Epoch: {}/{}... ".format(e+1, epochs),
+                      "Loss: {:.4f}".format(running_loss/print_every),
+                      "Test Loss: {:.3f}.. ".format(test_loss/len(valid_loader)),
+                      "Test Accuracy: {:.3f}".format(accuracy/len(valid_loader)))
+
+                running_loss = 0
+                
+            model.train()
+
+
+
+            # Implement a function for the validation pass
+
+def validation(model, valid_loader, criterion, device='cpu'):
+    """
+    Validated model during training
+    Intput: initialized model, dataloader for validation data,
+    criterion for loss calculation, device 'cpu' or 'cuda' as Strings
+    Output: test_loss and accuracy of model on validation data 
+    """
+    test_loss = 0
+    accuracy = 0
+    # Change devide
+    if device == 'cuda':
+        model.to('cuda')
+    for images, labels in valid_loader:
+        
+        if device == 'cuda':
+            images, labels = images.to('cuda'), labels.to('cuda')
+
+        output = model.forward(images)
+        test_loss += criterion(output, labels).item()
+
+        ps = torch.exp(output)
+        equality = (labels.data == ps.max(dim=1)[1])
+        accuracy += equality.type(torch.FloatTensor).mean()
+    
+    return test_loss, accuracy
+
+def test_network(model, test_loader, criterion, device='cpu'):
+    """
+    Evaluate model on data unseen during training
+    Intput: initialized model, dataloader for testing data,
+    criterion for loss calculation, device 'cpu' or 'cuda' as Strings
+    Output: test_loss and accuracy of model on test data 
+    """
+    model.eval()
+
+    test_loss = 0
+    accuracy = 0
+    # Change devide
+    if device == 'cuda':
+        model.to('cuda')
+    for images, labels in test_loader:
+        
+        if device == 'cuda':
+            images, labels = images.to('cuda'), labels.to('cuda')
+
+        output = model.forward(images)
+        test_loss += criterion(output, labels).item()
+
+        ps = torch.exp(output)
+        equality = (labels.data == ps.max(dim=1)[1])
+        accuracy += equality.type(torch.FloatTensor).mean()
+    
+    return test_loss, accuracy
+
+
+
+
+
+
+
+
+
+
