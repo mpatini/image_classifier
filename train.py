72eb8ecb1c86105f8aad9004259b0f576e874304
Michael Patini <michaelpatini@ucrwpa-4-5-10-25-112-44.wnet.ucr.edu>
2018-07-16 10:33:46 -0700

    created lib.args

--- train.py
+++ train.py
@@ -1,8 +1,12 @@
 
-
+from lib.dataload import load_dir, dataloader, labels
+from lib.improc import group_transform
+from lib.deeplearn import init_model, classifier, train_deep, validation, test_network
+from lib.checkpoint import save_checkpoint, load_checkpoint
+from lib.args import get_train_args
 
 
 if __name__ == '__main__':
-
+    
     
     criterion = nn.NLLLoss()
